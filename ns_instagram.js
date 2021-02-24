f939023872b092c2e3227ab07d217521db7aeba5
Zack Umar <zackumar@gmail.com>
2021-02-24 03:44:50 +0000

    Changed Name

---
+++ ns_instagram.js
@@ -0,0 +1,20 @@
+const Instagram = require('instagram-web-api')
+const FileCookieStore = require('tough-cookie-filestore2')
+
+const {INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD} = process.env
+
+const cookieStore = new FileCookieStore('./cookies.json')
+const client = new Instagram({username: INSTAGRAM_USERNAME, password: INSTAGRAM_PASSWORD , cookieStore})
+
+const argv = process.argv.slice(2)
+
+// ;(async () => {
+// 	await client.login()
+	
+// 	const photo = argv[0]
+// 	const caption = argv[1]
+
+// 	await client.uploadPhoto({ photo, caption: caption, post: 'feed' })
+// })()
+
+console.log(process.env.INSTAGRAM_USERNAME)
