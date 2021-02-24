49458883c15692576125a8738cd6e9835e722161
Zack Umar <zackumar@gmail.com>
2021-02-24 03:44:36 +0000

    Added Config

---
+++ ns_config.py
@@ -0,0 +1,11 @@
+instagram = {
+    "username": "YOUR USERNAME",
+    "password": "YOUR PASSWORD"
+}
+
+twitter = {
+    "consumer_key": "KEY",
+    "consumer_secret": "SECRET",
+    "access_token_key": "KEY",
+    "access_token_secret": "SECRET"
+}
