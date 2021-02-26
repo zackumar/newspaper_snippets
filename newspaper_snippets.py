71303772469f269c8e058514760d5e8bf4f51505
Zack Umar <zackumar@gmail.com>
2021-02-26 19:56:16 +0000

    Changed config to env vars

--- newspaper_snippets.py
+++ newspaper_snippets.py
@@ -9,6 +9,21 @@ import snippets
 import ns_config
 import ns_twitter
 
+import os
+
+instagram = {
+    "username": os.environ.get("instagram_username"),
+    "password": os.environ.get("instagram_password"),
+}
+
+twitter = {
+    "consumer_key": os.environ.get("twitter_consumer_key"),
+    "consumer_secret": os.environ.get("twitter_consumer_secret"),
+    "access_token_key": os.environ.get("twitter_access_token"),
+    "access_token_secret": os.environ.get("twitter_access_token_secret")
+}
+
+
 print(f'Running on {datetime.now()}...')
 
 caption = newspaper.downloadPDF()
@@ -28,10 +43,10 @@ cv2.imwrite('post.jpg', cropped_img)
 cap_arg = caption.replace('"', '\\"')
 
 os.popen(
-    f'node ./ns_instagram.js {ns_config.instagram["username"]} {ns_config.instagram["password"]} post.jpg "{cap_arg}"').read()
+    f'node ./ns_instagram.js {instagram["username"]} {instagram["password"]} post.jpg "{cap_arg}"').read()
 print("Posted on Instagram.")
 
-ns_twitter.postTwitter('post.jpg', caption)
+ns_twitter.postTwitter(twitter, 'post.jpg', caption)
 print("Posted on Twitter.")
 
 os.remove('post.jpg')
