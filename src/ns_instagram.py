60c2f8469e6814e8f277fcef5c6b04265a90546a
Zack Umar <zackumar@gmail.com>
2021-03-02 16:00:19 +0000

    Hotfix ns_instagram.py

--- src/ns_instagram.py
+++ src/ns_instagram.py
@@ -4,4 +4,4 @@ from instabot import Bot
 def postInstagram(instagram, filename, caption):
     bot = Bot()
     bot.login(username=instagram['username'], password=instagram['password'])
-    bot.upload_photo('./profilepic.jpg', caption='TEST')
+    bot.upload_photo(filename, caption='TEST')
