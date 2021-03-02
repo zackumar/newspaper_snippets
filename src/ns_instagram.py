527b4b9eff6a46c7b25e05185ff9831fb9f140c0
Zack Umar <zackumar@gmail.com>
2021-03-02 15:53:36 +0000

    Fixed instagram login issue

--- src/ns_instagram.py
+++ src/ns_instagram.py
@@ -3,5 +3,5 @@ from instabot import Bot
 
 def postInstagram(instagram, filename, caption):
     bot = Bot()
-    bot.login(username=username, password=password)
+    bot.login(username=instagram['username'], password=instagram['password'])
     bot.upload_photo('./profilepic.jpg', caption='TEST')
