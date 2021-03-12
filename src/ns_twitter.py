c14d6c95a313d34a4cd2d68a51ae5d595d8ff25d
Zack Umar <zackumar@gmail.com>
2021-03-12 23:01:03 +0000

    Changed disclaimer. Added disclaimer to twitter.

--- src/ns_twitter.py
+++ src/ns_twitter.py
@@ -1,12 +1,17 @@
 import tweepy
 
 
-def postTwitter(twitter, filename, caption):
+def postTwitter(twitter, filename, caption, disclaimer=False):
     auth = tweepy.OAuthHandler(
         twitter["consumer_key"], twitter["consumer_secret"])
     auth.set_access_token(
         twitter["access_token_key"], twitter["access_token_secret"])
     api = tweepy.API(auth)
 
-    media = api.media_upload(filename)
-    api.update_status(caption, media_ids=[media.media_id])
+    if (disclaimer):
+        disclaimer_img = api.media_upload('./images/disclaimer_lang.png')
+
+    post = api.media_upload(filename)
+
+    api.update_status(caption, media_ids=[
+                      disclaimer_img.media_id, post.media_id])
