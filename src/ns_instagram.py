784e2c0c686a23f306d04508632e0717fe61d324
Zack Umar <zackumar@gmail.com>
2021-05-21 16:53:00 +0000

    Ignore error. Bad but cant do much

--- src/ns_instagram.py
+++ src/ns_instagram.py
@@ -4,6 +4,11 @@ from instagrapi import Client
 def postInstagram(instagram, filename, caption):
     cl = Client()
     print('Logging in...')
-    cl.login(username=instagram['username'], password=instagram['password'])
+    try:
+        cl.login(username=instagram['username'],
+                 password=instagram['password'])
+    except e:
+        print(e)
+
     print('Uploading...')
     cl.photo_upload(filename, caption)
